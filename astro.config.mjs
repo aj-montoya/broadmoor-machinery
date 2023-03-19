452f5cb77f74e754c764bce33d4468cdced48593
AJ Montoya <ajmontoya516@gmail.com>
2023-03-19 19:06:52 -0400

    add bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    resolve: {
-      alias: {
-        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-      },
-    },
-  },
-});
+export default defineConfig({});
